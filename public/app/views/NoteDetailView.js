c7bf9781bf5f8e6af5ae77f791a7c4e49b4f9250
iansari <imran.iansari@gmail.com>
2011-11-10 00:00:33 -0500

    first hybrid draft with myContacts integration

---
+++ public/app/views/NoteDetailView.js
@@ -0,0 +1,20 @@
+﻿NotesApp.views.NoteDetailView = Ext.extend(Ext.Panel ,{
+         styleHtmlContent: true,
+         cls: 'detail',
+         tpl: [
+             '<h2>{contactName}</h2>',
+/*             '<div class="info">',
+                 '{address1}<br/>',
+                 '<img src="{rating_img_url_small}"/>',
+             '</div>',*/
+             '<div class="phone x-button">',
+                 '<a href="tel:{contactWorkPhone}">{contactWorkPhone}</a>',
+             '</div>'
+/*             '<div class="link x-button">',
+                 '<a href="{mobile_url}">Read more</a>',
+             '</div>'*/
+         ],
+         title: 'Info'
+     });
+
+
